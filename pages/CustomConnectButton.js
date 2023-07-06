75501f0b429e246d7454d796147b861f44984191
AerynQ <aeryn@xion.global>
2023-07-06 13:46:38 +0200

    customconnectbutton-simplication-test-android

--- pages/CustomConnectButton.js
+++ pages/CustomConnectButton.js
@@ -1,39 +1,15 @@
-import React, { useEffect } from "react";
-import { ConnectWallet, useDisconnect } from "@thirdweb-dev/react";
-import { useSwitchChain, useConnectionStatus } from "@thirdweb-dev/react";
+import React from "react";
+import { ConnectWallet } from "@thirdweb-dev/react";
+import { useSwitchChain } from "@thirdweb-dev/react";
 import { Polygon } from "@thirdweb-dev/chains";
 
 const CustomConnectButton = ({ buttonImage }) => {
-  const disconnect = useDisconnect();
-  const connectionStatus = useConnectionStatus();
   const switchChain = useSwitchChain();
 
-  // Save and restore connection state
-  useEffect(() => {
-    const pageLoaded = localStorage.getItem("pageLoaded") === "true";
-    const connectionStatusStored = localStorage.getItem("connectionStatus");
-
-    if (pageLoaded && disconnect && connectionStatusStored !== "connected") {
-      disconnect();
-      localStorage.setItem("pageLoaded", "false");
-    } else if (connectionStatusStored === "connected") {
-      switchChain(Polygon.chainId);
-    }
-
-    if (connectionStatus === "connected") {
-      switchChain(Polygon.chainId);
-    }
-  }, [connectionStatus, switchChain, disconnect]);
-
-  useEffect(() => {
-    const setPageLoaded = () => {
-      localStorage.setItem("pageLoaded", "true");
-      localStorage.setItem("connectionStatus", connectionStatus);
-    };
-    window.addEventListener("beforeunload", setPageLoaded);
-
-    return () => window.removeEventListener("beforeunload", setPageLoaded);
-  }, [connectionStatus]);
+  // Switch chain on component mount if not already on Polygon
+  React.useEffect(() => {
+    switchChain(Polygon.chainId);
+  }, [switchChain]);
 
   return (
     <div>
