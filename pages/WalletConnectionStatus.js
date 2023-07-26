f29d0ef3a022a53c1e0e9b592ed4eff76e896600
Aeryn Quarmby <67911815+aerynquarmby@users.noreply.github.com>
2023-07-26 15:38:44 +0200

    Update WalletConnectionStatus.js

--- pages/WalletConnectionStatus.js
+++ pages/WalletConnectionStatus.js
@@ -8,16 +8,16 @@ const WalletConnectionStatus = ({ approvalStatus }) => {
 
   useEffect(() => {
     const interval = setInterval(() => {
-      if (approvalStatus) {
+      if (approvalStatus.isApproved) { // Change this line
         setMessage('Successfully linked your crypto wallet for 1-click USDT payments on Scan to Pay. Redirecting...');
-      } else if (connectionStatus === 'connected' && !approvalStatus) { // Only show connected message if approvalStatus is false
+      } else if (connectionStatus === 'connected' && !approvalStatus.isApproved) { // Change this line
         setMessage('Open your crypto wallet, switch to Polygon Network & approve the USDT transaction...');
       } else {
         setMessage('');
       }
     }, 100);
     return () => clearInterval(interval);
-  }, [connectionStatus, approvalStatus]);
+  }, [connectionStatus, approvalStatus]); // Approval status is now an object, not a boolean
 
   return message && <p className={styles.message}>{message}</p>;
 };
